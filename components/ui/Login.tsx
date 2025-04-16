0e5adf673886746f51fc0f85018024e75023bfd8
hadi <121514937+dazbash1@users.noreply.github.com>
2025-04-16 10:52:04 +0330

    Update Login.tsx

--- components/ui/Login.tsx
+++ components/ui/Login.tsx
@@ -2,19 +2,21 @@ import { Field, Input, Box, HStack, VStack, Button } from "@chakra-ui/react";
 
 export default function Login() {
     return (
-        <VStack justifyContent='center' h='full' w='full' p={4} gap={2} bg='red.100'>
-            <Field.Root required>
-                <Field.Label>
-                    شماره تلفن <Field.RequiredIndicator />
-                </Field.Label>
-                <Input placeholder=".........09" />
-                <Field.HelperText>اطلاعات تماس شما محفوظ می باشد.</Field.HelperText>
-                <Field.ErrorText>شماره خود را صحیح وارد کنید</Field.ErrorText>
-            </Field.Root>
-            <Button colorPalette='red'>
-                ارسال کد
-            </Button>
-
+        <VStack bg='gray.500'>
+            <VStack h={64} w={48} p={3} bg='gray.300'>
+                <Field.Root required>
+                    <Field.Label>
+                        شماره تلفن <Field.RequiredIndicator />
+                    </Field.Label>
+                    <Input placeholder=".........09" />
+                    <Field.HelperText>لطفا شاره تماس خود را وارد کنید</Field.HelperText>
+                    <Field.ErrorText>شماره خود را صحیح وارد کنید</Field.ErrorText>
+                    <Button colorPalette='red'>
+                    ارسال کد
+                    </Button>
+                    <Field.HelperText>فراموشی رمز عبور</Field.HelperText>
+                </Field.Root>
+            </VStack>
         </VStack>
     )
 }
